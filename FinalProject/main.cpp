75f31fd7e487d85a417bbccffc14ad3dfbda0314
Johanes <Jron@Johaness-MacBook-Pro.local>
2016-10-28 11:02:00 +0700

    Initial Commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Johanes on 10/28/16.
+//  Copyright © 2016 Johanes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
